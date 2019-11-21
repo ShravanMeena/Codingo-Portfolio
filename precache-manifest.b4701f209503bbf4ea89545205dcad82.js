f8f894e8670ab9fb3256a25f1c72d016fc62680c
ShravanMeena <shravanmeena47@gmail.com>
2019-11-22 04:49:48 +0530

    Updates

---
+++ precache-manifest.b4701f209503bbf4ea89545205dcad82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15edafdf459c0567d08cc308e2a742f0",
+    "url": "/shravanmeena.github.io/Codingo-Portfolio/index.html"
+  },
+  {
+    "revision": "acd0946af976ca3e7e5e",
+    "url": "/shravanmeena.github.io/Codingo-Portfolio/static/css/main.f3539429.chunk.css"
+  },
+  {
+    "revision": "2914d294d0a6110bbd87",
+    "url": "/shravanmeena.github.io/Codingo-Portfolio/static/js/2.d31c3465.chunk.js"
+  },
+  {
+    "revision": "acd0946af976ca3e7e5e",
+    "url": "/shravanmeena.github.io/Codingo-Portfolio/static/js/main.c5edf824.chunk.js"
+  },
+  {
+    "revision": "b792b1b5e76d7a14be0a",
+    "url": "/shravanmeena.github.io/Codingo-Portfolio/static/js/runtime-main.75ce5afb.js"
+  },
+  {
+    "revision": "31f926ac134297d62f4ef885ff83c526",
+    "url": "/shravanmeena.github.io/Codingo-Portfolio/static/media/bg-callout.31f926ac.jpg"
+  },
+  {
+    "revision": "c934a8a6d299bfa55c48589e25982a8c",
+    "url": "/shravanmeena.github.io/Codingo-Portfolio/static/media/bg-masthead.c934a8a6.jpg"
+  }
+]);
